8df336a6364c46deee9853bb515ab4d298dcf446
Alex Miles Younger <alex@alexmilesyounger.com>
2015-07-01 15:35:28 -0400

    Code for string case challenge

---
+++ stringcases_solution.py
@@ -0,0 +1,6 @@
+def stringcase(string):
+	uppercased = string.upper()
+	lowercased = string.lower()
+	titlecased = string.title()
+	reverse = string[::-1]
+	return uppercased, lowercased, titlecased, reverse
